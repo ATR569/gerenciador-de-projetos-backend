ab5080337a337189fdbd7369502f124cea172834
Adson-Macedo <agnsoft@hotmail.com>
2020-12-10 21:58:17 -0300

    Modelos colaborador e projeto

--- src/main/java/com/backend/model/Colaborador.java
+++ src/main/java/com/backend/model/Colaborador.java
@@ -1,25 +1,42 @@
-// package com.backend.model;
+package com.backend.model;
 
-// // import javax.persistence.JoinColumn;
-// // import javax.persistence.ManyToOne;
-// import javax.persistence.Entity;
+import java.io.Serializable;
 
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
-// import lombok.NonNull;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
-// @Entity
-// @Builder
-// public class Colaborador {
-//     // @ManyToOne
-//     // // @JoinColumn(name="aluno_id")
-//     // private Aluno aluno;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-//     @NonNull
-//     private String papel;
-// }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Colaborador implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name="aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name="projeto_id")
+    private Projeto projeto;
+
+    private static final long serialVersionUID = 1L;
+
+    @NonNull
+    private String papel;
+}
