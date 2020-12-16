53bfc52e26524166b89eee25cee04535709843d9
Adson-Macedo <agnsoft@hotmail.com>
2020-12-16 10:33:19 -0300

    Mais sobre o bendito CORS

--- src/main/java/com/backend/controller/JwtAuthController.java
+++ src/main/java/com/backend/controller/JwtAuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="*")
 public class JwtAuthController {
 
     @Autowired
